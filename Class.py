9805b4ea0d7a88b9013a27aa7a7b69a4790016fd
Marcelle Lima <50150158+Cerry0101@users.noreply.github.com>
2025-08-13 11:03:48 -0300

    Update Class.py
    Commit Simples

--- Class.py
+++ Class.py
@@ -3,4 +3,5 @@ print("Primeira atividade da Aula 1 \n" \
 "1. Fazer o Óla mundo  \n" \
 "2. Fazer o envio para o Github  \n" \
 "3. Fazer uma edição  \n" \
-"4. Baixar novamente para o Desktop  \n")
+
+"4. Baixar novamente para o Desktop!  \n")
