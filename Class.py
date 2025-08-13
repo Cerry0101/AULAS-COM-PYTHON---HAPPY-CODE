f60c5822c1bac62b4ddf23437a5d8838aae0bfe3
Marcelle <marcellesilva0101@outlook.com>
2025-08-13 11:27:18 -0300

    minhas alterações

--- Class.py
+++ Class.py
@@ -4,4 +4,4 @@ print("Primeira atividade da Aula 1 \n" \
 "2. Fazer o envio para o Github  \n" \
 "3. Fazer uma edição  \n" \
 
-"4. Baixar novamente para o Desktop!  \n")
+"4. Baixar novamente para o Desktop  \n")
